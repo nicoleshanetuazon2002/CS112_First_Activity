855ad2f67ad126bb49c329c99927f648eaebcec2
nicoleshanetuazon2002 <144906962+nicoleshanetuazon2002@users.noreply.github.com>
2023-09-13 04:49:22 -0700

    Add files via upload

---
+++ CS112_TUAZON_FIRST_ACTIVITY.py
@@ -0,0 +1,8 @@
+print("Complete Name : Nicole Shane P. Tuazon")
+print("Course : Bachelor of Science in Computer Science")
+print("Section : BSCS - CS1E")
+print("Age : 20 years old")
+print("Birthday : September 28, 2002")
+print("Complete Home Address : Brgy. Camaman-an Buntong Gater Cagayan de Oro City")
+
+
